a67819a22da6412830229fbeead6cc1e1c834189
Tyler Stodden <tylersstodden@gmail.com>
2023-12-01 19:35:14 -0500

    Add a test that an exception is raised when no digits are present

--- aoc2023/day_1/test_part_1.py
+++ aoc2023/day_1/test_part_1.py
@@ -1,3 +1,4 @@
+import pytest
 from aoc2023.day_1.part_1 import get_calibration_value
 
 
@@ -15,3 +16,10 @@ def test_getting_a_calibration_value_for_one_digit() -> None:
     got = get_calibration_value(line)
 
     assert got == 11
+
+
+def test_raising_an_exception_when_no_digits_exist() -> None:
+    line = "abcdef"
+
+    with pytest.raises(Exception, match="No digit is present in 'abcdef'."):
+        get_calibration_value(line)
