e99e3082ee877c60ead0d1f139cbbb412960b8a0
Tyler Stodden <tylersstodden@gmail.com>
2023-12-01 19:30:54 -0500

    Implement the function to get a calibration value from input text

--- aoc2023/day_1/part_1.py
+++ aoc2023/day_1/part_1.py
@@ -1,2 +1,15 @@
 def get_calibration_value(line: str) -> int:
-    return 0
+    """
+    Return an integer containing a calibration value for the input line.
+    :param line: A string containing the input text.
+    """
+    digits = extract_digits(line)
+    return int(digits[0] + digits[-1])
+
+
+def extract_digits(line: str) -> [str]:
+    result = []
+    for char in line:
+        if char.isdigit():
+            result.append(char)
+    return result
