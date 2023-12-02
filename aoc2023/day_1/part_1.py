d60c96fb4005f0947b5a8f5f32ba0b133eafed41
Tyler Stodden <tylersstodden@gmail.com>
2023-12-01 19:49:37 -0500

    Add logic for finding the sum of the calibration values from the input

--- aoc2023/day_1/part_1.py
+++ aoc2023/day_1/part_1.py
@@ -1,3 +1,11 @@
+def run() -> None:
+    with open("input/input.txt", "r") as f:
+        all_lines = f.readlines()
+
+    calibration_values = [get_calibration_value(line) for line in all_lines]
+    print(f"Sum of all calibration values is: '{sum(calibration_values)}'.")
+
+
 def get_calibration_value(line: str) -> int:
     """
     Return an integer containing a calibration value for the input line.
@@ -15,3 +23,7 @@ def extract_digits(line: str) -> [str]:
         if char.isdigit():
             result.append(char)
     return result
+
+
+if __name__ == "__main__":
+    run()
