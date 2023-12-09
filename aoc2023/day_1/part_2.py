026278bdc9ef6bb45873db83edd677ca78c71263
Tyler Stodden <tylersstodden@gmail.com>
2023-12-09 12:08:06 -0500

    Refactor the Day 1, Part 2 functions to take the trie as an argument

--- aoc2023/day_1/part_2.py
+++ aoc2023/day_1/part_2.py
@@ -1,38 +1,39 @@
 from aoc2023.day_1.digit_trie import DigitTrie
 
-_DIGIT_TERMS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+DIGIT_TERMS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 
 def run() -> None:
     with open("input/input.txt", "r") as f:
         all_lines = f.readlines()
 
-    calibration_values = [get_calibration_value(line) for line in all_lines]
+    trie = get_populated_trie(DIGIT_TERMS)
+    calibration_values = [get_calibration_value(line, trie) for line in all_lines]
     print(f"Sum of all calibration values is: '{sum(calibration_values)}'.")
 
 
-def get_calibration_value(line: str) -> int:
+def get_calibration_value(line: str, trie: DigitTrie) -> int:
     """
     Return an integer containing a calibration value for the input line.
 
     :param line: A string containing the input text.
+    :param trie: A trie storing the available vocabulary.
     """
-    digits = extract_digits(line)
+    digits = extract_digits(line, trie)
     if len(digits) == 0:
         raise Exception(f"No digit is present in '{line}'.")
     return int(digits[0] + digits[-1])
 
 
-def extract_digits(line: str) -> [str]:
+def extract_digits(line: str, trie: DigitTrie) -> [str]:
     result = []
-    trie = _get_prefilled_digit_trie(_DIGIT_TERMS)
     for i in range(0, len(line)):
-        if digit := _extract_digit(trie, line[i:]):
+        if digit := _extract_digit(line[i:], trie):
             result.append(digit)
     return result
 
 
-def _extract_digit(trie: DigitTrie, text: str) -> str | None:
+def _extract_digit(text: str, trie: DigitTrie) -> str | None:
     first_letter, *_ = text
     if first_letter.isdigit():
         return first_letter
@@ -41,7 +42,7 @@ def _extract_digit(trie: DigitTrie, text: str) -> str | None:
     return None
 
 
-def _get_prefilled_digit_trie(terms: [str]) -> DigitTrie:
+def get_populated_trie(terms: [str]) -> DigitTrie:
     trie = DigitTrie()
     for t in terms:
         trie.insert(t)
--- aoc2023/day_1/test_part_2.py
+++ aoc2023/day_1/test_part_2.py
@@ -1,5 +1,10 @@
 import pytest
-from aoc2023.day_1.part_2 import get_calibration_value, extract_digits
+from aoc2023.day_1.part_2 import (
+    get_calibration_value,
+    get_populated_trie,
+    extract_digits,
+    DIGIT_TERMS,
+)
 
 
 @pytest.mark.parametrize(
@@ -11,29 +16,34 @@ from aoc2023.day_1.part_2 import get_calibration_value, extract_digits
     ],
 )
 def test_extracting_digits(line, expected):
-    got = extract_digits(line)
+    trie = get_populated_trie(DIGIT_TERMS)
+
+    got = extract_digits(line, trie)
 
     assert got == expected
 
 
 def test_getting_a_calibration_value_for_two_digits() -> None:
+    trie = get_populated_trie(DIGIT_TERMS)
     line = "abconedef2"
 
-    got = get_calibration_value(line)
+    got = get_calibration_value(line, trie)
 
     assert got == 12
 
 
 def test_getting_a_calibration_value_for_one_digit() -> None:
+    trie = get_populated_trie(DIGIT_TERMS)
     line = "abconedef"
 
-    got = get_calibration_value(line)
+    got = get_calibration_value(line, trie)
 
     assert got == 11
 
 
 def test_raising_an_exception_when_no_digits_exist() -> None:
+    trie = get_populated_trie(DIGIT_TERMS)
     line = "abcdef"
 
     with pytest.raises(Exception, match="No digit is present in 'abcdef'."):
-        get_calibration_value(line)
+        get_calibration_value(line, trie)
